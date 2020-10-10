8a529b59c4b90e4100bd1eb2e6e0c87bd7ec5108
Mo Wong <mowong1987@gmail.com>
2020-10-10 10:38:51 +0200

    feat: router set up

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,20 @@
 import * as express from 'express';
 import * as dotenv from 'dotenv';
 
+import * as router from './router';
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 
+app.use(router)
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
 app.listen(port, () => {
-  console.log(`Server running on https://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port}`)
 })
