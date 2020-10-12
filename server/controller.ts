9f69467d8d6dbdc8d7751ab9c6fc0cedad9624f1
Mo Wong <mowong1987@gmail.com>
2020-10-12 13:46:48 +0200

    feat: refactored router and controller

---
+++ server/controller.ts
@@ -0,0 +1,15 @@
+import {v4 as uuidv4} from 'uuid';
+
+const redirect = (req: any, res: any) => {
+  res.redirect(`/${uuidv4()}`);
+}
+
+const render = (req: any, res: any) => {
+  const room = req.params.room;
+  res.render('room', {roomId: room});
+}
+
+module.exports = {
+  redirect,
+  render
+}
--- server/router.ts
+++ server/router.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
-import * as path from 'path';
-import {v4 as uuidv4} from 'uuid';
 
 const router = Router();
+const controller = require('./controller');
 
-router.get('/', (req, res)=> {
-  res.redirect(`/${uuidv4()}`);
-});
+router.get('/', controller.redirect);
 
-router.get('/:room', (req, res) => {
-  res.render('room', {roomId: req.params.room})
-});
-
+router.get('/:room', controller.render);
 
 module.exports = router;
