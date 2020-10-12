a21e68d6034f063968e59ffb191b175964f7f796
Justin Patterson <PJ943@yahoo.com>
2020-10-12 12:37:16 +0200

    feat(client): add basic apollo client

--- client/src/graphql/Client.ts
+++ client/src/graphql/Client.ts
@@ -1,4 +1,7 @@
-import { ApolloCLient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import gql from 'graphql-tag';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import gql from 'graphql';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
