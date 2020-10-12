c1f2564bef090adf88b9c34d88cffa52ba651fda
Mo Wong <mowong1987@gmail.com>
2020-10-12 12:13:27 +0200

    feat: refactoring express server with apollo server

--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,45 @@
+import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
 import dotenv from 'dotenv';
-import { Server } from 'http';
-dotenv.config();
 import socketio from 'socket.io';
+import { Server } from 'http';
+
 const router = require('./router');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+dotenv.config();
 
 const app = express();
 
-const server = new Server(app);
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
 const io = socketio(server);
 
 const port: number = Number(process.env.PORT);
 
-app.set('view engine', 'ejs');
-
-// app.get('/', (req, res)=>{
-//   res.send('Hello old friends!')
-// });
-app.use(express.static('public'));
 app.use(router);
 
 server.listen(port, ()=> {
 console.log(`Server now running at port ${port}`)
 })
 
-export { app };
+
+
+
+// // app.set('view engine', 'ejs');
+
+// app.use(express.static('public'));
+
+// server.listen(port, ()=> {
+// console.log(`Server now running at port ${port}`)
+// })
+
+// io.on('connection', socket => {
+//   socket.on('join-room', (roomId, userId) => {
+//     // console.log(roomId, userId)
+//     socket.join(roomId)
+//     socket.to(roomId).broadcast.emit('user connected', userId)
+//   })
+// })
