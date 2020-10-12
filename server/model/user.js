7b251d39ede33f0dc715fff23b521e3881d2868f
Mo Wong <mowong1987@gmail.com>
2020-10-12 20:38:37 +0200

    feat: refactored User sql object

--- server/model/user.js
+++ server/model/user.js
@@ -1,10 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    bio: DataTypes.STRING,
-    avatar: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      required: true
+    },
+    bio: {
+      type: DataTypes.STRING,
+      required: false
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      required: false
+    }
   });
+
+  User.associate = db => {
+    db.User.hasMany(db.Data);
+  };
 
   return User;
 };
