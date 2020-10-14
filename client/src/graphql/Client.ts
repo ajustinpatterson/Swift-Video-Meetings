f8691da17a1ad2e322ac314f0edcddab828b138c
Mo Wong <mowong1987@gmail.com>
2020-10-14 19:31:06 +0200

    feat: refactored client.ts

--- client/src/graphql/Client.ts
+++ client/src/graphql/Client.ts
@@ -1,11 +1,23 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 import gql from 'graphql';
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: 'https://48p1r2roz4.sse.codesandbox.io',
+//   cache: new InMemoryCache(),
+// });
 
 // const query = gql`{}`;
+
+const link = new HttpLink({
+  uri: 'http://localhost:3002/'
+})
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+})
 
 export default client;
