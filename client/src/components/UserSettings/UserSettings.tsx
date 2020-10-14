2ab753d23b0530300356be5d935feff44a8e1f8a
Mo Wong <mowong1987@gmail.com>
2020-10-14 18:32:29 +0200

    feat: useState on userSettings

--- client/src/components/UserSettings/UserSettings.tsx
+++ client/src/components/UserSettings/UserSettings.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const [ name, setName ] = useState<string>('');
+const [ bio, setBio ] = useState<string>('');
+const [ avatar, setAvatar ] = useState<string>('');
+const [ status, setStatus ] = useState<string>('');
 
 const UserSettings = () => {
-  return <div></div>;
+  return <div>
+    Hello World
+  </div>;
 };
 
 export default UserSettings;
--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect, useRef } from 'react';
 import Peer from 'peerjs';
 import Meeting from '../Meeting/Meeting';
 import './App.css';
-import Landing from '../../components/Landing/Landing';
+// import Landing from '../../components/Landing/Landing';
+import UserSettings from '../../components/UserSettings/UserSettings';
 
 interface props {
   socket: any;
 }
 
 function App({ socket }: props) {
-  return <Landing />;
+  return <UserSettings />;
 }
 
 export default App;
