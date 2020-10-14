3decc6d8e8edcf93e33c740f83806a3923da6feb
Justin Patterson <PJ943@yahoo.com>
2020-10-14 15:05:18 +0200

    feat(client): add login functionality to landing page using google auth
    button changes based on loggedIn status

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import logo from '../../assets/swift-logo.png';
 import './Landing.css';
@@ -7,30 +7,41 @@ const Landing = () => {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [userName, setUserName] = useState('');
 
-  const responseGoogle = (response: any) => {
-    console.log(response);
-    if (response) {
-      setUserName(response.profileObj.name);
+  const responseGoogle = async (response: any) => {
+    try {
+      console.log(response);
+      if (response) {
+        setUserName(response.profileObj.name);
+        setLoggedIn(true);
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
-  return !loggedIn ? (
+  return (
     <div className="landing">
       <img src={logo} alt="Logo" />
-      <GoogleLogin
-        className="button"
-        clientId="770694473973-nsm7s39sp1tvm3jpg6d3pk7ln309gvbr.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-    </div>
-  ) : (
-    <div className="landing">
-      <img src={logo} alt="Logo" />
-
-      <button className="button">Start Meeting</button>
+      {!loggedIn ? (
+        <GoogleLogin
+          clientId="770694473973-nsm7s39sp1tvm3jpg6d3pk7ln309gvbr.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              className="button"
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              Login
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+      ) : (
+        <button className="button">Start Meeting</button>
+      )}
     </div>
   );
 };
