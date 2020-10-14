a3389c6759a94f5f8d3b856b2e8874edacfc1ef1
Mo Wong <mowong1987@gmail.com>
2020-10-14 10:42:22 +0200

    fix: env and docker files

--- server/model/db.ts
+++ server/model/db.ts
@@ -3,11 +3,7 @@ import UserFactory from "./user";
 import dotenv from 'dotenv';
 dotenv.config;
 
-const database = process.env.DB_NAME;
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
-
-let sequelize = new Sequelize(database, 'postgres', 'postgres', {
+let sequelize = new Sequelize('zoomapp', 'postgres', 'postgres', {
   dialect: 'postgres',
 });
 
