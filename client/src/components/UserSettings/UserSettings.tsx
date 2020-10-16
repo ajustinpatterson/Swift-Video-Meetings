739867860471ab8f15622384d9b0ce7f2cc32b80
Mo Wong <mowong1987@gmail.com>
2020-10-16 21:12:41 +0200

    feat: more UserSettings features

--- client/src/components/UserSettings/UserSettings.tsx
+++ client/src/components/UserSettings/UserSettings.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useMutation, gql, useQuery } from '@apollo/client';
+import { updateLanguageServiceSourceFile } from 'typescript';
 // import { User }  from './User';
 
 export default function UserSettings (): JSX.Element {
@@ -16,7 +17,16 @@ export default function UserSettings (): JSX.Element {
     }
   `
 
+  const UPDATE_USER = gql `
+    mutation UpdateUser {
+      updateUser {
+
+      }
+    }
+  `
+
   const {data, loading, error} = useQuery(GET_USERS);
+  const {updateUser} = useMutation(UPDATE_USER);
 
   console.log(data)
 
@@ -36,42 +46,56 @@ export default function UserSettings (): JSX.Element {
     status: ''
   })
 
+  useEffect(() => {
+    setUser({name: data.name})
+  }, []);
+
+  const setModal = () => {
+
+  };
+
+  const onSubmit = (input) => {
+    setModal(false);
+    updateUser({
+
+    })
+  };
+
   return (
-    <div>
+    <div className="user-settings-container">
       <div>
-        Change Your User Settings
+        Account Settings
       </div>
 
       <div className="edit-image">
         <h3>Image</h3>
-        <h4>My mugshot</h4>
+        <h4>{user.image} My mugshot</h4>
         <button>Edit</button>
       </div>
 
       <div className="edit-name">
         <h3>Name</h3>
-        <h4>Mo</h4>
+        <h4>{user.name} Mo</h4>
         <button>Edit</button>
       </div>
 
       <div className="edit-email">
         <h3>Email</h3>
-        <h4>mo@gmail.com</h4>
+        <h4>{user.email} mo@gmail.com</h4>
         <button>Edit</button>
       </div>
 
       <div className="edit-bio">
         <h3>Bio</h3>
-        <h4>Just a small town girl living in a lonely world</h4>
+        <h4>{user.bio} Just a small town girl living in a lonely world</h4>
         <button>Edit</button>
       </div>
 
       <div className="edit-status">
         <h3>Status</h3>
-        <h4>Swifting so hard</h4>
+        <h4>{user.status} Swifting so hard</h4>
         <button>Edit</button>
       </div>
-
 
     </div>
   )
