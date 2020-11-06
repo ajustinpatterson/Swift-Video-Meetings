f266d4099cea1712ccf6e861c40cd44dbce40551
Mo Wong <mowong1987@gmail.com>
2020-11-06 13:35:08 +0100

    fix: css width

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -3,15 +3,16 @@ import { useQuery } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 import { GET_USERS } from '../../graphql/Queries';
 import './UserProfile.scss';
+
 export default function UserProfile(): JSX.Element {
+
   const { data, loading, error } = useQuery(GET_USERS);
+
   const history = useHistory();
 
-
-  function handleSettings (){
+  function handleSettings () {
     history.push('/userSettings')
-  }
-
+  };
 
   return (
     <div className="container">
@@ -32,33 +33,6 @@ export default function UserProfile(): JSX.Element {
           <div className="color-box"></div>
         </div>
       </div>
-
-
-
-
-      {/* <div className="upper-container">
-        <div className="upper-main">
-          {/* <div className="greeting">
-            Hello, {data?.getUsers[0]?.givenName}
-          </div> */}
-          {/* <div className="profile-pic">
-            <img src={data?.getUsers[0]?.imageUrl} />
-          </div>
-        </div>
-      </div>
-
-      <div className="lower-container">
-        <div className="lower-main">
-          <div>{data?.getUsers[0]?.status}</div>
-          <div>{data?.getUsers[0]?.email}</div>
-          <div>{data?.getUsers[0]?.bio}</div>
-          <button className="btn" onClick={handleSettings}>
-            Edit profile
-          </button>
-        </div>
-      </div> */}
-
-
     </div>
   );
 }
